dcefe233b5652ee4e496a70d8b36926fdca80f74
Sarah H <sarahrose.henderson@gmail.com>
2019-08-29 23:55:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
-def reverse_string(string)
-string
+original_array = string.split(" ")
+return_array = []
+original_array.each do|string|
+  return_array << string.reverse
 end
-
-puts reverse_string("Hello there, and how are you? ")
+return_array.join(" ")
+end
